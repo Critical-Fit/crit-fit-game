a79cbd5b14302f4d9a4c595fa97683004860d2e0
medium0fry <medium0fry@gmail.com>
2021-06-30 20:29:09 -0400

    Added prototype page to list of pages tested

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,12 @@
 import './App.css';
-import Register from './pages/register/Register';
-import Login from './pages/login/Login';
+// import Register from './pages/register/Register';
+// import Login from './pages/login/Login';
+import Prototype from './pages/prototype/Prototype';
 
 function App() {
 	return (
 		<div>
-			<Login />
+			<Prototype />
 		</div>
 	);
 }
